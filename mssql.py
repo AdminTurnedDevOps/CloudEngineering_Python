83a6b5b96f3d9e726e410f65f5655cff0bd051fc
Mike Levan <mlevan1992@gmail.com>
2020-01-14 08:31:38 -0500

    Connect to MSSQL with Windows auth

---
+++ mssql.py
@@ -0,0 +1,35 @@
+import pyodbc
+import logging
+import sys
+
+def analytics_db_connect(sqlserver, database):
+    try:
+        conn = pyodbc.connect('Driver={SQL Server};'
+                            f'Server={sqlserver};'
+                            f'Database={database};'
+                            'Trusted_Connection=yes;'
+                            )
+
+        cursor = conn.cursor()
+    except Exception as e:
+        logging.error(msg='An error has occurred while connecting to your SQL server...')
+        print(e)
+
+    try:
+        query = input("Please enter a query to run: ")
+        cursor.execute(query)
+        
+    except Exception as e:
+        logging.error(msg='An error has occurred running a query on your database...')
+        print(e)
+
+    for t in cursor:
+        if t is None:
+            logging.error(msg='Query returned no data...')
+        else:
+            print(t)
+
+sqlserver = sys.argv[1]
+database = sys.argv[2]
+
+analytics_db_connect(sqlserver, database)
